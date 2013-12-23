70c492cadac24d5eafd187f4cad0b5edfbc2f9f3
jackey <jziwenchen@gmail.com>
2013-12-23 22:17:17 +0800

    API

---
+++ api/protected/controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+class UserController extends Controller
+{
+	/**
+	 * Declares class-based actions.
+	 */
+	public function actions()
+	{
+		
+	}
+    
+    public function actionIndex() {
+      print "hello world";
+      die();
+    }
+}
