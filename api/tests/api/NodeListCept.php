b20c0f052f6dfeb99ff7cb7c80b3f0e219609950
jackey <jziwenchen@gmail.com>
2014-01-04 22:16:04 +0800

    .

--- api/tests/api/NodeListCept.php
+++ api/tests/api/NodeListCept.php
@@ -1,8 +1,8 @@
 <?php
 $I = new ApiGuy($scenario);
 $I->wantTo('Get Node List');
-$I->sendGet('node/list&type=photo');
+$I->sendGet('node/list?type=photo');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContains('http error');
+$I->seeResponseContains('"success":true');
 ?>
--- api/tests/api/PostPhotoCept.php
+++ api/tests/api/PostPhotoCept.php
@@ -1,7 +1,10 @@
 <?php
 $I = new ApiGuy($scenario);
 $I->wantTo('Post Photo');
-$I->sendPost('node/post');
+$I->sendPost('node/post', array(
+    "description" => "I am from test",
+    "photo" => "@/home/jackey/Pictures/test.png"
+));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContains('http error');
