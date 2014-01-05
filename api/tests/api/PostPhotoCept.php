bd4517321e77f39e18ce6acb594a095933b49cf6
tonysh518 <tony@fuel-it-up.com>
2014-01-05 11:45:11 +0800

    test

--- api/tests/api/PostPhotoCept.php
+++ api/tests/api/PostPhotoCept.php
@@ -1,9 +1,9 @@
 <?php
 $I = new ApiGuy($scenario);
 $I->wantTo('Post Photo');
-$I->sendPost('node/post', array(
+$I->sendPost('/node/post', array(
     "description" => "I am from test",
-    "photo" => "@/home/jackey/Pictures/test.png"
+    "photo" => "@/Applications/MAMP/htdocs/bank_wall/admin/app/photo/1.jpg"
 ));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
