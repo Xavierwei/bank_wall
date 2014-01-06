d7fc179ff68c0e41a025415f862c83503b11aeea
tonysh518 <tony@fuel-it-up.com>
2014-01-06 13:06:30 +0800

    update

--- admin/app/js/services.js
+++ admin/app/js/services.js
@@ -6,4 +6,4 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 var SGWallAdminServices = angular.module('SGWallAdmin.services', []).
-  value('ROOT', 'http://localhost/bank_wall/api/index.php');
+  value('ROOT', 'http://localhost:8888/bank_wall/api/index.php');
