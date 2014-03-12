adc71ac2fc7ab50607ed5f616d5ab3898d811488
Jackey <jziwenchen@gmail.com>
2014-03-12 21:11:52 +0800

    run.sh

--- PyGmail/run.sh
+++ PyGmail/run.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 #python2.7 gmail.py
 
-n=$( ps -ef | grep "python gmail" | wc -l)
 
-# 判断： 如果没有进程在跑脚本 就直接删除.lock
+n=$( ps -ef | grep -v grep | grep "gmail.py" | wc -l)
+
 if [[ n -eq 1 ]]; then
-	rm .lock
+       exit 
 fi
 
 # 运行gmail.py
 /usr/local/bin/python2.7 /var/www/vhosts/polyardshanghai.com/httpdocs/sgwall/PyGmail/gmail.py  1>logs/log.`date "+%Y-%m-%d:%H:%M:%S"`.txt
-
-
