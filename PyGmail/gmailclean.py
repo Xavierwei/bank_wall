7e1694ab5d1889019026c862f39cce0358b55010
Jackey <jziwenchen@gmail.com>
2014-03-16 23:12:42 +0800

    .

--- PyGmail/gmailclean.py
+++ PyGmail/gmailclean.py
@@ -8,12 +8,21 @@ import gmail
 root = os.path.abspath(os.path.dirname(__file__))
 
 def load_mail_ids():
-  paths = os.listdir(os.path.join(root, "caches"))
+
+  dirs = ["inbox", "[Gmail]/Spam", "."]
   ids = []
-  for path in paths:
-    ids.append(path)
-    
+  for dir_path in dirs:
+    cache_dir = os.path.join(root, "caches", dir_path)
+    if os.path.isdir(cache_dir):
+      paths = os.listdir(cache_dir)
+      for path in paths:
+        try:
+          int(path)
+        except:
+          continue
+        ids.append(path)
   return ids
+
 
 def connect_gmail():
   return gmail.reconnect_gmail(dict(config.items("mailaccount"))["user"], dict(config.items("mailaccount"))["pass"])
