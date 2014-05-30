5bb2fd4bf8c35eb34619f6c56d66f8c6a93d9647
tonysh518 <tony@fuel-it-up.com>
2014-05-31 07:46:31 +0800

    update

--- include/tpl.php
+++ include/tpl.php
@@ -48,6 +48,7 @@
 				<div data-a="lang" data-d="lang=fr" class="btn language-item language-item-fr"><p class="fr"></p></div>
 				<div data-a="lang" data-d="lang=en" class="btn language-item language-item-en"><p class="en"></p></div>
 			</div>
+			<a class="term-icon" target="_blank" href="doc/Wall150ans_CGU_V3_EN.doc"></a>
 		</div>
 		<!--  -->
 		<div class="content">
